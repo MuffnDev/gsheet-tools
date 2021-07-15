ee75d74acca50efff14e4303cfd1779f77868e03
Hubert Grossin <hubert.grossin@gmail.com>
2021-07-16 01:21:42 +0200

    Fix GSheet output

--- pack.js
+++ pack.js
@@ -10,30 +10,85 @@ const REMOVE_LINES_IF_INCLUDES = [
   'exports.'
 ];
 
+const DISCLAIMERS = {
+  js: `/**
+ * WARNING: This file is generated from the GSheet Tools project: https://gitlab.com/muffin-dev/nodejs/gsheet-tools
+ * Any changes made directy from the Google Apps Script editor will be erased on the next release.
+ */`,
+  html: `<!-- WARNING: This file is generated from the GSheet Tools project: https://gitlab.com/muffin-dev/nodejs/gsheet-tools
+ Any changes made directy from the Google Apps Script editor will be erased on the next release. -->`
+};
+
+const IMPORT_REGEX = /const (.+) = require/;
+
 (async () => {
 
   // Get scripts from ./gsheet directory
   let files = await NodeHelpers.readdirAsync(INPUT_DIR, false, true, 'js', true, true);
 
+  // For each file
   for (const file of files) {
+    // Get the file content as a string
     const js = await NodeHelpers.readFileAsync(file.path);
-    const split = js.split(/\r?\n/);
-    for (let i = 0; i < split.length; i++) {
+    // Split file lines
+    const lines = js.split(/\r?\n/);
+
+    const imports = [];
+
+    // For each line
+    for (let i = 0; i < lines.length; i++) {
+      // If the line contains one of the "remove conditions", clean it from the output
       for (const rm of REMOVE_LINES_IF_INCLUDES) {
-        if (split[i].includes(rm)) {
-          split[i] = '';
+        if (lines[i].includes(rm)) {
+          lines[i] = '';
           break;
+        }
+      }
+
+      // Checks if the line declares an import (which is not available in Google Apps Script, all functions are in global space)
+      const match = lines[i].match(IMPORT_REGEX);
+      if (match) {
+        imports.push(match[1]);
+        lines[i] = '';
+        continue;
+      }
+
+      // If the current line contains an import, remove it
+      for (const imp of imports) {
+        while (lines[i].includes(imp + '.')) {
+          lines[i] = lines[i].replace(imp + '.', '');
         }
       }
     }
 
-    await NodeHelpers.writeFileAsync(Path.join(OUTPUT_DIR, file.fullName), split.join('\n').trim())
+    // Remove all lines before the first comment
+    // for (let i = 0; i < lines.length; i++) {
+    //   if (!lines[i].startsWith('/**')) {
+    //     lines.splice(i, 1);
+    //     i--;
+    //   }
+    //   else {
+    //     break;
+    //   }
+    // }
+
+    // Adds the disclaimer for Google Apps Script export
+    lines.unshift(DISCLAIMERS.js, '');
+    // Write the final file
+    await NodeHelpers.writeFileAsync(Path.join(OUTPUT_DIR, file.fullName), lines.join('\n').trim())
   }
 
   // Copy other files (*.json and *.html) as is into the output /gsheet directory
   files = await NodeHelpers.readdirAsync(INPUT_DIR, false, true, [ 'json', 'html' ], true, true);
   for (const file of files) {
-    await NodeHelpers.copyFileAsync(file.path, Path.join(OUTPUT_DIR, file.fullName));
+    if (file.extension === 'html') {
+      const html = await NodeHelpers.readFileAsync(file.path);
+      html = DISCLAIMERS.html + '\n\n';
+      await NodeHelpers.writeFileAsync(Path.join(OUTPUT_DIR, file.fullName), html)
+    }
+    else {
+      await NodeHelpers.copyFileAsync(file.path, Path.join(OUTPUT_DIR, file.fullName));
+    }
   }
 
 })();
